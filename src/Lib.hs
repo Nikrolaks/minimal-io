2d7d6a36a5768f68be6db11309eb5f97b241526b
Nikrolaks <nikrolaks17@gmail.com>
2023-05-24 01:41:14 +0300

    second stage

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,80 +13,132 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
-module Lib
-    ( Expression,
-        someFunc
-    ) where
+module Lib (
+    Expression,
+    StringTree,
+    AssociativePrecision,
+    ShowP,
+    someFunc
+) where
 
 import GHC.Generics
+import Text.Show
+import Data.Typeable
+import Data.Foldable
 --import Generics.Deriving
 
-data Expression a = V | CE a | (Expression a) :* (Expression a) deriving (Show, Generic)
+data Expression a = Z | S (Expression a) | P {} | Q {e :: (Expression a)} | CE a | (Expression a) :* (Expression a) | W {w1 :: a, w2 :: a, w3 :: a} deriving (Show, Generic)
 
-class ShowP f where
-    showP :: String -> f a -> String
+data StringTree = L String | Ch String StringTree | R String StringTree StringTree deriving Show
 
--- If we have a parameter ‘p’, we just map over it, as expected:
+data AssociativePrecision = InfixAP Int | InfixlAP Int | InfixrAP Int
 
-instance ShowP Par1 where
-    showP _ (Par1 p) = "Par1 p"
+class ShowP a where
+    devTree :: a -> StringTree
 
--- The same with ‘Rec1’ (just use ‘fmap’ because the inner part is a ‘Functor’):
+    --- LAW showsPAPrec ap x r ++ s == showsPAPrec ap x (r ++ s)
+    showsPAPrec :: AssociativePrecision -> a -> ShowS
+    showsPAPrec _ x s = showP x s
+    
+    showP :: a -> ShowS
+    showP x = showsPAPrec (InfixAP 11) x
 
-instance ShowP f => ShowP (Rec1 f) where
-    showP _ (Rec1 a) = "Rec1 a"
+instance ShowP (Par1 p) where
+    devTree = const . L $ "Par1"
+    showP (Par1 p) = showString "not implemented"
 
---instance (ShowP f, Constructor f) => ShowP (Rec1 f) where
---    showP _ r = conName $ unRec1 r
+instance ShowP (f p) => ShowP (Rec1 f p) where
+    devTree = const . L $ "Rec1"
+    showP (Rec1 a) = showString "not implemented"
 
--- A constructor without fields only can be returned untouched:
+instance ShowP (U1 p) where
+    devTree = const . L $ "U1"
+    showP U1 = id
 
-instance ShowP U1 where
-  showP s U1 = s
+instance Show c => ShowP (K1 i c p) where
+    devTree = const . L $ "K1"
+    showP k1@(K1 e) = showString $ show e
 
--- A field that is not ‘p’ parameter should not change:
+instance (ShowP (a p), ShowP (b p), ShowP (c p), ShowP ((:*:) b c p)) => ShowP ((:*:) a ((:*:) b c) p) where
+    devTree (a :*: b) = R ":*:" (devTree a) (devTree b)
+    -- so dirty hack
+    showP (a :*: b) s = showP a "" ++ s ++ showP b s
 
-instance Show c => ShowP (K1 i c) where
-    --showP _ :: K1 i c p -> String
-    showP s k1 = s ++ "uyt" ++ (show . unK1 $ k1)
+instance (ShowP (a p), ShowP (b p)) => ShowP ((:*:) a b p) where
+    devTree (a :*: b) = R ":*:" (devTree a) (devTree b)
+    -- so dirty hack
+    showP (a :*: b) s = showP a "" ++ s ++ showP b ""
 
--- Metadata has no effect, just unwrap it and continue with the inner value,
--- if the inner value is an instance of ‘Functor’:
-
-instance (Constructor c, ShowP f) => ShowP (C1 c f) where
-    showP _ c1@(M1 a) = case conFixity c1 of
-        Prefix -> showP cn a
-        Infix _ n -> show n ++ showP cn a
-        where
+instance (Constructor c, ShowP (a p), ShowP (b p), ShowP (((:*:) a b p))) => ShowP (C1 c ((:*:) a b) p) where
+    devTree c1@(M1 a) = Ch "C1" $ devTree a
+    showP c1@(M1 a) =
+        let
             cn = conName c1
+            next = showP a
+        in
+        case conFixity c1 of
+            -- record 
+            Prefix -> showString $ cn ++ " {" ++ next ", " ++ "}"
+            Infix _ _ -> showString . next $ " " ++ cn ++ " "
 
-instance (ShowP f) => ShowP (D1 c f) where
-    showP _ d1@(M1 a) = showP "d1" a
+instance (Constructor c, ShowP (U1 p)) => ShowP (C1 c U1 p) where
+    devTree c1@(M1 a) = Ch "C1" $ devTree a
+    -- only Prefix
+    showP c1@(M1 a) = showString . conName $ c1
 
-instance (Selector c, ShowP f) => ShowP (S1 c f) where
-    showP s s1@(M1 a) = showP (s ++ selName s1 ++ "x3") a
+instance (Constructor c, ShowP (f p)) => ShowP (C1 c f p) where
+    devTree c1@(M1 a) = Ch "C1" $ devTree a
+    -- only Prefix
+    showP c1@(M1 a) =
+        let
+            cn = conName c1
+            next = showP a
+        in if conIsRecord c1
+           then showString $ cn ++ " {" ++ next "" ++ "}"
+           else showString (cn ++ " ") . next
 
-instance ShowP f => ShowP (M1 i c f) where
-    showP _ (M1 a) = showP "etoty" a
+instance ShowP (f p) => ShowP (D1 c f p) where
+    devTree d1@(M1 a) = Ch "D1" $ devTree a
+    showP d1@(M1 a) = showP a
 
--- When we have a sum, we should try to map what we get, provided that it
--- contains something that has ‘Functor’ instance:
+instance (Selector c, ShowP (f p)) => ShowP (S1 c f p) where
+    devTree s1@(M1 a) = Ch "S1" $ devTree a
+    showP s1@(M1 a) | length sn > 0 = showString (sn ++ " = ") . next
+                    | otherwise = next
+        where sn = selName s1
+              next = showP a
 
-instance (ShowP l, ShowP r) => ShowP (l :+: r) where
-    showP s (L1 a) = showP (s ++ "L") a
-    showP s (R1 a) = showP (s ++ "R") a
+instance ShowP (f p) => ShowP (M1 i c f p) where
+    devTree m1@(M1 a) = Ch "M1" $ devTree a
+    showP m1@(M1 a) = showP a
 
--- The same for products:
-
-instance (ShowP a, ShowP b) => ShowP (a :*: b) where
-    showP s (a :*: b) = showP "" a ++ " " ++ s ++ " " ++ showP "" b
-
+instance (ShowP (l p), ShowP (r p)) => ShowP ((:+:) l r p) where
+    devTree x = case x of
+        L1 a -> Ch "L1" $ devTree a
+        R1 a -> Ch "R1" $ devTree a
+    showP x = case x of
+        L1 a -> showP a
+        R1 a -> showP a
 
 infixl 3 :*
 
-someFunc :: IO ()
+printRes test = showP (from test) $ " !!! " ++ (show $ devTree (from test))
+
 someFunc = do
-    let e = CE 1 :* V
-    let t = from e
-    putStrLn $ showP "" t
+    --let test1 = Z :: Expression Int
+    --let test2 = CE 3 :: Expression Int
+    --let test3 = CE 2 :* Z :: Expression Int
+    --let test4 = W 4 3 7 :: Expression Int
+    --let test5 = P :: Expression Int
+    --let test6 = Q test2
+    --putStrLn $ printRes test1
+    --putStrLn $ printRes test2
+    --putStrLn $ printRes test3
+    --putStrLn $ printRes test4
+    --putStrLn $ printRes test5
+    --putStrLn $ printRes test6
+    --print . fromM1 . from $ test4
+
+
