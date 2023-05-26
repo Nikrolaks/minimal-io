7a5cfa292f8bc377e54031a7589e6e79196a5a2d
Nikrolaks <nikrolaks17@gmail.com>
2023-05-26 03:38:39 +0300

    let is my truly love

--- src/Read.hs
+++ src/Read.hs
@@ -63,134 +63,136 @@ checkIsRightPosed (InfixrAP _) True = True
 checkIsRightPosed _ _ = False
 
 class ReadP a where
-    readsPAPrec :: AssociativePrecision -> Bool -> a -> ReadS a
+    readsPAPrec :: AssociativePrecision -> Bool -> ReadS a
 
 class ReadP a => ReadProd a where
-    readProd :: AssociativePrecision -> Bool -> String -> a -> ReadS a
+    readProd :: AssociativePrecision -> Bool -> String -> ReadS a
     readProd ap rp d = readsPAPrec ap rp
 
 instance ReadP Int where
-    readsPAPrec ap rp _ = readsPrec (getPrecision ap)
+    readsPAPrec ap rp = readsPrec (getPrecision ap)
 
 instance ReadP p => ReadP (Par1 p) where
-    readsPAPrec ap rp _ s = [(Par1 x, t) | (x, t) <- readsPAPrec ap rp (undefined :: p) s]
+    readsPAPrec ap rp s = [(Par1 x, t) | (x, t) <- readsPAPrec ap rp s]
 
 instance ReadP (f p) => ReadP (Rec1 f p) where
-    readsPAPrec ap rp _ s = [(Rec1 a, t) | (a, t) <- readsPAPrec ap rp (undefined :: f p) s]
+    readsPAPrec ap rp s = [(Rec1 a, t) | (a, t) <- readsPAPrec ap rp s]
 
 instance ReadP (U1 p) where
-    readsPAPrec _ _ _ s = [(U1, s)]
+    readsPAPrec _ _ s = [(U1, s)]
 
 instance ReadP c => ReadP (Rec0 c p) where
-    readsPAPrec ap rp _ s = [(K1 e, t) | (e, t) <- readsPAPrec ap rp (undefined :: c) s]
+    readsPAPrec ap rp s = [(K1 e, t) | (e, t) <- readsPAPrec ap rp s]
 
 -- dummy
 instance (ReadP (a p), ReadP (b p)) => ReadP ((:*:) a b p) where
-    readsPAPrec ap _ _ s = [(a :*: b, r) |
-                            (a, t) <- readsPAPrec ap rp (undefined :: a p) s,
-                            (b, r) <- readsPAPrec ap (not rp) (undefined :: b p) t]
-        where rp = checkIsRightPosed ap False
+    readsPAPrec = undefined
 
 instance (ReadProd (a p), ReadProd (b p)) => ReadProd ((:*:) a b p) where
-    readProd ap _ d _ s = [(a :*: b, r) |
-                           (a, t) <- readProd ap rp d (undefined :: a p) s,
-                           (d, u) <- lex t,
-                           (b, r) <- readProd ap (not rp) d (undefined :: b p) u]
+    readProd ap _ d s = [(a :*: b, r) |
+                         (a, t) <- readProd ap rp d s,
+                         (d, u) <- lex t,
+                         (b, r) <- readProd ap (not rp) d u]
         where rp = checkIsRightPosed ap False
 
 instance (Constructor c, ReadP ((:*:) a b p), ReadProd (a p), ReadProd (b p), ReadProd ((:*:) a b p)) => ReadP (C1 c ((:*:) a b) p) where
-    readsPAPrec ap rp dm =
+    readsPAPrec ap rp =
         let
-            cn :: String
-            cn = conName dm
-
-            cf :: Fixity
-            cf = conFixity dm
-
-            myAP :: AssociativePrecision
-            myAP = gF2AP cf
-            
-            next d = readProd myAP uselessRP d (unM1 dm)
+            (shouldMy, res) =
+                let
+                    cn = conName . fst $ res "" !! 0
+                    cf = conFixity . fst $ res "" !! 0
+                    myAP = gF2AP cf
+                in (
+                    shouldParen ap rp (gF2AP . conFixity . fst $ res "" !! 0),
+                    case cf of
+                        Prefix -> if (conIsRecord . fst $ res "" !! 0)
+                                  then \s -> [(M1 a, r) |
+                                              (cn, t) <- lex s,
+                                              ("{", u) <- lex t,
+                                              (a, v) <- readProd myAP uselessRP ", " u,
+                                              ("}", r) <- lex v]
+                                  else \s -> [(M1 a, r) |
+                                              (cn, t) <- lex s,
+                                              (a, r) <- readProd myAP uselessRP " " t]
+                        _ -> \s -> [(M1 a, r) | (a, r) <- readProd myAP uselessRP cn s])
         in
-            readParen (shouldParen ap rp myAP) $ 
-                case cf of
-                    Prefix -> if conIsRecord dm
-                              then \s -> [(M1 a, r) |
-                                          (cn, t) <- lex s,
-                                          ("{", u) <- lex t,
-                                          (a, v) <- next ", " u,
-                                          ("}", r) <- lex v]
-                              else \s -> [(M1 a, r) |
-                                          (cn, t) <- lex s,
-                                          (a, r) <- next " " t]
-                    _ -> \s -> [(M1 a, r) | (a, r) <- next cn s]
+            readParen shouldMy res
 
 instance (Constructor c, ReadP (U1 p)) => ReadP (C1 c U1 p) where
     -- only Prefix
-    readsPAPrec _ _ dm s =
+    readsPAPrec _ _ s =
         let
-            cn = conName dm
+            res = 
+                let
+                    cn = conName . fst $ res !! 0
+                in
+                    [(M1 U1, r) | (cn, r) <- lex s]
         in
-            [(M1 U1, r) | (cn, r) <- lex s]
+            res
 
 instance (Constructor c, ReadP (f p)) => ReadP (C1 c f p) where
     -- only Prefix
-    readsPAPrec ap rp dm =  
+    readsPAPrec ap rp =  
         let
-            cn :: String
-            cn = conName dm
-
-            next = readsPAPrec prefixAP uselessRP (unM1 dm)
+            (shouldMy, res) =
+                let
+                    cn = conName . fst $ res "" !! 0
+                in (
+                    shouldParen ap rp (gF2AP . conFixity . fst $ res "" !! 0),
+                    if (conIsRecord . fst $ res "" !! 0)
+                    then \s -> [(M1 a, r) |
+                                (cn, t) <- lex s,
+                                ("{", u) <- lex t,
+                                (a, v) <- readsPAPrec prefixAP uselessRP u,
+                                ("}", r) <- lex v]
+                    else \s -> [(M1 a, r) |
+                                (cn, t) <- lex s,
+                                (a, r) <- readsPAPrec prefixAP uselessRP t])
         in
-            readParen (shouldParen ap rp . gF2AP . conFixity $ dm) $
-                \s -> (
-                    if conIsRecord dm
-                    then [(M1 a, r) |
-                          (cn, t) <- lex s,
-                          ("{", u) <- lex t,
-                          (a, v) <- next u,
-                          ("}", r) <- lex v]
-                    else [(M1 a, r) |
-                          (cn, t) <- lex s,
-                          (a, r) <- next t])
+            readParen shouldMy res
 
 instance ReadP (f p) => ReadP (D1 c f p) where
-    readsPAPrec ap rp _ s = [(M1 a, r) | (a, r) <- readsPAPrec ap rp (undefined :: f p) s]
+    readsPAPrec ap rp s = [(M1 a, r) | (a, r) <- readsPAPrec ap rp s]
 
 instance (Selector c, ReadP (f p)) => ReadP (S1 c f p) where 
-    readsPAPrec ap rp dm s = (
-            if length sn > 0
-            then [(M1 a, r) |
-                  (sn, t) <- lex s,
-                  ("=", u) <- lex t,
-                  (a, r) <- readsPAPrec uselessAP uselessRP (undefined :: f p) u]
-            else [(M1 a, r) | (a, r) <- readsPAPrec ap rp (undefined :: f p) s])
-        where
-            sn = selName dm
+    readsPAPrec ap rp s = 
+        let
+            res = 
+                let
+                    sn = selName (fst $ res !! 0)
+                in
+                    if length sn > 0
+                    then [(M1 a, r) |
+                          (sn, t) <- lex s,
+                          ("=", u) <- lex t,
+                          (a, r) <- readsPAPrec uselessAP uselessRP u]
+                    else [(M1 a, r) | (a, r) <- readsPAPrec ap rp s]
+        in res
 
 instance (Selector c, ReadP (f p)) => ReadProd (S1 c f p) where
 
 instance ReadP (f p) => ReadP (M1 i c f p) where
-    readsPAPrec ap rp _ s = [(M1 a, r) | (a, r) <- readsPAPrec ap rp (undefined :: f p) s]
+    readsPAPrec ap rp s = [(M1 a, r) | (a, r) <- readsPAPrec ap rp s]
 
 instance (ReadP (l p), ReadP (r p)) => ReadP ((:+:) l r p) where
-    readsPAPrec ap rp _ s =
-        [(L1 a, u) | (a, u) <- readsPAPrec ap rp (undefined :: l p) s] ++
-        [(R1 b, v) | (b, v) <- readsPAPrec ap rp (undefined :: r p) s]
+    readsPAPrec ap rp s =
+        [(L1 a, u) | (a, u) <- readsPAPrec ap rp s] ++
+        [(R1 b, v) | (b, v) <- readsPAPrec ap rp s]
 
 data Exa = Z | I Int Exa deriving (Eq, Show, Read, Generic)
 
 instance ReadP Exa where
-    readsPAPrec ap rp x s = [(to a, t) | (a, t) <- readsPAPrec ap rp (from x) s]
+    readsPAPrec ap rp s = [(to a, t) | (a, t) <- readsPAPrec ap rp s]
 
 someFunc2 :: IO ()
 someFunc2 = do
     let test1 = "Z"
     let test2 = "I 3 Z"
     let test3 = "I 3 (I 2 Z)"
-    let res1 = readsPAPrec uselessAP uselessRP (undefined :: Exa) test1
-    let res2 = readsPAPrec uselessAP uselessRP (undefined :: Exa) test2
-    let res3 = readsPAPrec uselessAP uselessRP (undefined :: Exa) test3
+    let res1 = readsPAPrec uselessAP uselessRP test1
+    let res2 = readsPAPrec uselessAP uselessRP test2
+    let res3 = readsPAPrec uselessAP uselessRP test3
     print $ (Z, "") `elem` res1
     print $ (I 3 Z, "") `elem` res2
     print $ (I 3 (I 2 Z), "") `elem` res3
